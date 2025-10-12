7a8438fa1de5641a4e4062efc771fa67af2022c7
jeet7122 <jeetthakkar2612@gmail.com>
2025-10-11 21:09:47 -0400

    #7 Added patients Table

---
+++ src/main/resources/tables/patients.sql
@@ -0,0 +1,11 @@
+CREATE TABLE patients(
+    id INT PRIMARY KEY ,
+    first_name VARCHAR(50) NOT NULL ,
+    last_name VARCHAR(50),
+    age INT NOT NULL,
+    gender VARCHAR(20) NOT NULL,
+    address VARCHAR(255),
+    blood_group VARCHAR(5),
+    has_chronic_disease BOOLEAN,
+    registration_date TIMESTAMP DEFAULT current_timestamp
+);
