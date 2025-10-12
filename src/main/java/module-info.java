0b2e9afeb2f7be1fd030301992858f989564d3ba
jeet7122 <jeetthakkar2612@gmail.com>
2025-10-11 22:02:47 -0400

    #13 DB Configuration is all set

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,9 +1,6 @@
 module org.example.hms {
     requires javafx.controls;
-    requires javafx.fxml;
     requires java.sql;
 
-
-    opens org.example.hms to javafx.fxml;
     exports org.example.hms;
 }
--- src/main/java/org/example/hms/MainScreenApplication.java
+++ src/main/java/org/example/hms/MainScreenApplication.java
@@ -2,11 +2,26 @@ package org.example.hms;
 
 import javafx.application.Application;
 import javafx.stage.Stage;
+import org.example.hms.utils.DatabaseConnector;
 
 import java.io.IOException;
+import java.sql.Connection;
 
 public class MainScreenApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
+        try {
+            Connection c = DatabaseConnector.getConnection();
+            if(c != null){
+                System.out.println("Connected to database.");
+            }
+            else{
+                System.out.println("Database not connected.");
+            }
+            DatabaseConnector.closeConnection();
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
