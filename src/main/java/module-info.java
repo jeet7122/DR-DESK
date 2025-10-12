2d485f41d82b84e4dffeb88b3fb9c15c6099de10
jeet7122 <jeetthakkar2612@gmail.com>
2025-10-11 21:54:49 -0400

    #13 Created database connector class and added getConnection() method to connect to db

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module org.example.hms {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.sql;
 
 
     opens org.example.hms to javafx.fxml;
---
+++ src/main/java/org/example/hms/utils/DatabaseConnector.java
@@ -0,0 +1,32 @@
+package org.example.hms.utils;
+
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class DatabaseConnector {
+    private static Connection connection;
+    private DatabaseConnector() {}
+
+    public static Connection getConnection() throws SQLException, ClassNotFoundException {
+        if (connection == null) {
+            try(InputStream inputStream = DatabaseConnector.class.getResourceAsStream("/config.properties")) {
+                Properties prop = new Properties();
+                prop.load(inputStream);
+
+                String url = prop.getProperty("db.url");
+                String username = prop.getProperty("db.user");
+                String password = prop.getProperty("db.password");
+                connection = DriverManager.getConnection(url, username, password);
+                System.out.println("Database connection established");
+            }
+            catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return connection;
+    }
+    
+}
