56d8f3724da83c8ab33a54d75eabce24a1e192d8
jeet7122 <jeetthakkar2612@gmail.com>
2025-10-11 21:21:44 -0400

    #9 Added Medical_Records Table

---
+++ src/main/resources/tables/medical_records.sql
@@ -0,0 +1,8 @@
+CREATE TABLE medical_records(
+    id SERIAL PRIMARY KEY,
+    patient_id INT,
+    condition VARCHAR(50),
+    description VARCHAR(255),
+    date_diagnosed DATE DEFAULT current_timestamp,
+    current_status VARCHAR(50)
+);
