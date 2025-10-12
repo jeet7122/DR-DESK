2b3627d473f0d72da056b6de02044e589cf21303
jeet7122 <jeetthakkar2612@gmail.com>
2025-10-11 21:31:10 -0400

    #11 FK Referenced

--- src/main/resources/tables/Appointment.sql
+++ src/main/resources/tables/Appointment.sql
@@ -4,5 +4,7 @@ CREATE TABLE appointment(
     patient_id INT,
     status VARCHAR(20),
     appointment_date_and_time TIMESTAMP,
-    notes VARCHAR(255)
+    notes VARCHAR(255),
+    foreign key (doctor_id) REFERENCES doctors(doctor_id),
+    foreign key (patient_id) REFERENCES patients(id)
 );
