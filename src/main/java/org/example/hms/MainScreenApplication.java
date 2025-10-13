4bd36f9e39ffcdd14c6feecda9fef41f9735b72e
jeet7122 <jeetthakkar2612@gmail.com>
2025-10-13 16:43:13 -0400

    #15 Test Passed for Medicals records create and get records

--- src/main/java/org/example/hms/MainScreenApplication.java
+++ src/main/java/org/example/hms/MainScreenApplication.java
@@ -2,44 +2,21 @@ package org.example.hms;
 
 import javafx.application.Application;
 import javafx.stage.Stage;
-import org.example.hms.models.Department;
-import org.example.hms.tests.DepartmentTests;
+import org.example.hms.models.MedicalRecord;
+import org.example.hms.tests.MedicalRecordsTest;
+
 
 import java.io.IOException;
-import java.util.List;
 
 public class MainScreenApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-
-        Department department = new Department();
-        department.setDepartmentName("Cardiology");
-        department.setDepartmentDescription("Handles diagnosis and treatment of heart-related conditions");
-        department.setLocation("Floor 2");
-        DepartmentTests.AddDepartmentTest(department);
-
-        Department departmentToUpdate = new Department();
-        departmentToUpdate.setDepartmentName("Neurology");
-        departmentToUpdate.setDepartmentDescription("Specializes in disorders of the nervous system");
-        departmentToUpdate.setLocation("Floor 3");
-        boolean updated = DepartmentTests.updateDepartment(departmentToUpdate, 2);
-        if (updated){
-            System.out.println("Department update Test Passed");
-        }
-        else {
-            System.out.println("Department update Test Failed");
-        }
-
-        boolean deleted = DepartmentTests.deleteDepartment(3);
-        if (deleted){
-            System.out.println("Department delete Test Passed");
-        }
-        else {
-            System.out.println("Department delete Test Failed");
-        }
-
-        List<Department> departments = DepartmentTests.getAllDepartments();
-        departments.forEach(System.out::println);
-
+        MedicalRecord medicalRecord = new MedicalRecord();
+        medicalRecord.setPatientId(6);
+        medicalRecord.setCondition("Cold and Cough");
+        medicalRecord.setDescription("has a little bit of cold and soar throat");
+        medicalRecord.setCurrentStatus("Normal");
+        MedicalRecordsTest.AddMedicalRecordTest(medicalRecord);
+        MedicalRecordsTest.getAllMedicalRecordsTest();
     }
 }
--- src/main/java/org/example/hms/tests/MedicalRecordsTest.java
+++ src/main/java/org/example/hms/tests/MedicalRecordsTest.java
@@ -1,4 +1,20 @@
 package org.example.hms.tests;
 
+import org.example.hms.dao.MedicalRecordsDAO;
+import org.example.hms.models.MedicalRecord;
+
+import java.util.List;
+
 public class MedicalRecordsTest {
+
+    public static void AddMedicalRecordTest(MedicalRecord medicalRecord){
+        MedicalRecordsDAO.AddMedicalRecord(medicalRecord);
+    }
+
+    public static void getAllMedicalRecordsTest(){
+        List<MedicalRecord> records =  MedicalRecordsDAO.getMedicalRecords();
+        for(MedicalRecord medicalRecord : records){
+            System.out.println(medicalRecord);
+        }
+    }
 }
