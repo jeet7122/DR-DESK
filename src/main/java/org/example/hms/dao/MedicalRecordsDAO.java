e71c1aaa53dffd0af989f21774ca8cf641200b1b
jeet7122 <jeetthakkar2612@gmail.com>
2025-10-13 16:48:15 -0400

    #18 Added Update method for Medical_Records in DAO

--- src/main/java/org/example/hms/dao/MedicalRecordsDAO.java
+++ src/main/java/org/example/hms/dao/MedicalRecordsDAO.java
@@ -6,6 +6,7 @@ import org.example.hms.utils.DatabaseConnector;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
@@ -68,5 +69,38 @@ public class MedicalRecordsDAO {
         }
 
         return medicalRecords;
+    }
+
+    public static void updateMedicalRecord(MedicalRecord medicalRecord, int id){
+
+        String query = """
+                UPDATE medical_records
+                SET patient_id = ?, condition = ?, description = ?, current_status = ?
+                WHERE id = ?
+        """;
+
+        try(
+                Connection connection = DatabaseConnector.getConnection();
+                PreparedStatement statement = connection.prepareStatement(query)
+                )
+        {
+            statement.setInt(1, medicalRecord.getPatientId());
+            statement.setString(2, medicalRecord.getCondition());
+            statement.setString(3, medicalRecord.getDescription());
+            statement.setString(4, medicalRecord.getCurrentStatus());
+            statement.setInt(5, id);
+            int updated = statement.executeUpdate();
+            if(updated > 0)
+            {
+                System.out.println("Record updated successfully");
+            }
+            else
+            {
+                System.out.println("Failed to update medical record");
+            }
+        }
+        catch (Exception e) {
+            System.out.println("SQLException: " +  e.getMessage());
+        }
     }
 }
