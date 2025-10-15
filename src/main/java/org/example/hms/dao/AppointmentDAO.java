0782b789a794a01b9ddfee06087dc1dc915f9808
jeet7122 <jeetthakkar2612@gmail.com>
2025-10-14 21:52:44 -0400

    #20 defined getAll() method

--- src/main/java/org/example/hms/dao/AppointmentDAO.java
+++ src/main/java/org/example/hms/dao/AppointmentDAO.java
@@ -1,12 +1,12 @@
 package org.example.hms.dao;
 
 import org.example.hms.models.Appointment;
+import org.example.hms.models.Status;
 import org.example.hms.utils.DatabaseConnector;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
-import java.sql.Types;
+import java.sql.*;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
 public class AppointmentDAO implements GenericDAO<Appointment> {
@@ -54,6 +54,34 @@ public class AppointmentDAO implements GenericDAO<Appointment> {
 
     @Override
     public List<Appointment> getAll() {
-        return List.of();
+        List<Appointment> appointments = new ArrayList<>();
+        String query = """
+                SELECT * FROM appointment
+        """;
+
+        try
+                (Connection con = DatabaseConnector.getConnection();
+                 PreparedStatement ps = con.prepareStatement(query);
+                 ResultSet rs = ps.executeQuery())
+        {
+
+            while (rs.next()) {
+                Appointment obj = new Appointment();
+                obj.setAppointmentId(rs.getInt("appointment_id"));
+                obj.setDoctorId(rs.getInt("doctor_id"));
+                obj.setPatientId(rs.getInt("patient_id"));
+                obj.setAppointmentDate(rs.getObject("appointment_date_and_time", LocalDateTime.class));
+                obj.setNotes(rs.getString("notes"));
+                obj.setStatus(rs.getObject("status", Status.class));
+                appointments.add(obj);
+            }
+        }
+        catch (SQLException e){
+            System.out.println("SQLException: "  + e.getMessage());
+        }
+        catch (Exception e) {
+            System.out.println("Exception: "  + e.getMessage());
+        }
+        return appointments;
     }
 }
