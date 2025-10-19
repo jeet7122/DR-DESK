ac378cb56aa07d6ecbd058f0dcf8e24315325a7c
jeet7122 <jeetthakkar2612@gmail.com>
2025-10-19 01:01:35 -0400

    #15 All test passed for updating appointment

--- src/main/java/org/example/hms/MainScreenApplication.java
+++ src/main/java/org/example/hms/MainScreenApplication.java
@@ -2,17 +2,32 @@ package org.example.hms;
 
 import javafx.application.Application;
 import javafx.stage.Stage;
+import org.example.hms.dto.AppointmentDTO;
+import org.example.hms.models.Appointment;
 import org.example.hms.models.Billing;
+import org.example.hms.models.Status;
+import org.example.hms.tests.AppointmentTest;
 import org.example.hms.tests.BillingTest;
 
 
 import java.io.IOException;
+import java.time.LocalDateTime;
 
 
 public class MainScreenApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        BillingTest test = new BillingTest();
-        test.TestGetAll();
+        AppointmentDTO appointmentDTO = new AppointmentDTO();
+        AppointmentTest test = new AppointmentTest();
+        Mappers mappers = new Mappers();
+        appointmentDTO.setAppointmentDate(LocalDateTime.now().plusDays(2) );
+        appointmentDTO.setNotes("Update Test");
+        appointmentDTO.setDoctorId(1);
+        appointmentDTO.setPatientId(8);
+        appointmentDTO.setStatus(String.valueOf(Status.SCHEDULED));
+
+        Appointment appointment = mappers.mapFromDTO(appointmentDTO);
+        test.updateAppointment(appointment, 4);
+
     }
 }
